92be34dec866e2953d3a2598293d0732ea944861
Lior Hatiel <127618295+liorhatiel@users.noreply.github.com>
2024-07-19 17:48:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,78 @@
-'use strict';
-
-
-
+"use strict";
 
 // Declaration:
 
 // Declare Links
+const homeLink = document.getElementById("homeLink");
 const aboutLink = document.getElementById("aboutLink");
 const webLink = document.getElementById("webLink");
-const gameLink = document.getElementById("gameLink");
 
-// Declare 'Projects' Button.
+// Declare 'Projects' Button
 const projectLink = document.getElementById("btnToProject");
 
-
 // Declare Sections
-const aboutSection = document.querySelector('.about_section');
-const webSection = document.querySelector('.web_development_section');
-const gameSection = document.querySelector('.game_development_section');
+const aboutSection = document.querySelector(".about_section");
+const webSection = document.querySelector(".web_development_section");
 
 // Declare Logo
-const logo = document.getElementById('logo');
+const logo = document.getElementById("logo");
+
+// Declare Lior Copyright
+const liorCopy = document.getElementById("lior_copy");
 
 // Declare Toggle Button
 const toggleButton = document.querySelector(".toggle-button");
-const navbarLinks = document.querySelector('.header_links');
-const actualLinks = document.querySelectorAll('.header_link_item');
+const navbarLinks = document.querySelector(".header_links");
+const actualLinks = document.querySelectorAll(".header_link_item");
 
-toggleButton.addEventListener('click' , () =>
-navbarLinks.classList.toggle('active'));
-
-
-
-actualLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        navbarLinks.classList.remove('active');
-    });
+// Toggle navbar visibility on button click
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
 });
 
-
-
-
-// Click the logo to scroll to the top of the page
-logo.addEventListener('click', () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth' 
+// Close the navbar when a link is clicked
+actualLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navbarLinks.classList.remove("active");
   });
 });
 
+// Scroll to the top of the page when the logo is clicked
+logo.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
 
-// scrollIntoView -> Scroll the element before the dot into the visible area of the browser window:
-const scrollToAboutSection = aboutLink.addEventListener('click' , function(e)
-{
-    e.preventDefault();
-    aboutSection.scrollIntoView( {behavior: 'smooth' } );
-})
+// Scroll to the top of the page when the copyright is clicked
+liorCopy.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
 
-const scrollToWebSection = webLink.addEventListener('click' , function(e)
-{
-    e.preventDefault();
-    webSection.scrollIntoView( {behavior: 'smooth' } );
-})
+// Scroll to the respective sections
+homeLink.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
 
-const scrollToGameSection = gameLink.addEventListener('click' , function(e)
-{
-    e.preventDefault();
-    gameSection.scrollIntoView( {behavior: 'smooth' } );
-})
+aboutLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  aboutSection.scrollIntoView({ behavior: "smooth" });
+});
 
+webLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  webSection.scrollIntoView({ behavior: "smooth" });
+});
 
-
-// Activate 'Projects' Button:
-const goToProject = projectLink.addEventListener('click' , function(e)
-{
-    e.preventDefault();
-    webSection.scrollIntoView( {behavior: 'smooth'} );
-})
-
-
-
+// Scroll to the 'Projects' section
+projectLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  webSection.scrollIntoView({ behavior: "smooth" });
+});
